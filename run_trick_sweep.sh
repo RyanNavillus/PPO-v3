d591b741419d75343c2cb087d1c5c6140d5a7d89
Ryan Sullivan <ryanpnavillus@gmail.com>
2023-03-15 19:37:53 -0400

    Update wandb entity and add seed argument to sweep script

--- run_trick_sweep.sh
+++ run_trick_sweep.sh
@@ -1,54 +1,55 @@
-timesteps=10000000
-env_id="MsPacman-v5"
+timesteps=10000
+env_id="MsPacman-v5 Pong-v5"
+seeds=2
 workers=2
 git-tag="v0.0.1-22-g4f9ce4b"
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_none --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_all --symlog True --two-hot True --percentile-scale True --critic-ema --unimix 0.01 --critic-zero-init True --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_symlog --symlog True --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_twohot --two-hot True --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_percentile --percentile-scale True --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_criticema --critic-ema True --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_unimix --unimix 0.01 --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
     --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_zeroinit --critic-zero-init True --total-timesteps $timesteps --track" \
-    --num-seeds 3 \
+    --num-seeds $seeds \
     --workers $workers
 
 python -m openrlbenchmark.rlops \
