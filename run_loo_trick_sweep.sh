5578f2f351227be77863be39bf3523f9a8413cb1
Ryan Sullivan <ryanpnavillus@gmail.com>
2023-03-16 14:27:56 -0400

    Replace LOO for two-hot with two-hot+critic-ema

--- run_loo_trick_sweep.sh
+++ run_loo_trick_sweep.sh
@@ -12,7 +12,7 @@ poetry run python -m cleanrl_utils.benchmark \
 
 poetry run python -m cleanrl_utils.benchmark \
     --env-ids $env_id \
-    --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_all_notwohot --symlog True --percentile-scale True --critic-ema True --unimix 0.01 --critic-zero-init True --total-timesteps $timesteps --track" \
+    --command "poetry run python ppo_v3/ppo_envpool_tricks.py --exp-name ppo_envpool_tricks_all_notwohot_nocriticema --symlog True --percentile-scale True --unimix 0.01 --critic-zero-init True --total-timesteps $timesteps --track" \
     --num-seeds $seeds \
     --workers $workers
 
@@ -45,7 +45,7 @@ poetry run python -m openrlbenchmark.rlops \
     --filters '?we=openrlbenchmark&wpn=baselines&ceik=env&cen=exp_name&metric=charts/episodic_return' 'baselines-ppo2-cnn' \
     --filters '?we=ryan-colab&wpn=PPO-v3&ceik=env_id&cen=exp_name&metric=charts/episodic_return' \
     "ppo_envpool_tricks_nosymlog?tag=${gittag}" \
-    "ppo_envpool_tricks_notwohot?tag=${gittag}" \
+    "ppo_envpool_tricks_notwohot_nocriticema?tag=${gittag}" \
     "ppo_envpool_tricks_nopercentile?tag=${gittag}" \
     "ppo_envpool_tricks_nocriticema?tag=${gittag}" \
     "ppo_envpool_tricks_nounimix?tag=${gittag}" \
